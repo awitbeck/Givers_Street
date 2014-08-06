903b9ce52a060a86b686dbc331b679d581e40c3a
Anne Witbeck <awitbeck@yahoo.com>
2014-08-06 04:06:58 -0400

    fourth commit

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@
 $(document).foundation();
 
 
-//redirect on
+//redirect on main page keyword search
 
 $("#input1").keyup(function(event) {
                 if (event.keyCode==13) {
@@ -19,11 +19,11 @@ $("#input1").keyup(function(event) {
 });
 
 
-
-// Doughnut Chart for Weekly Goals
-
+//redirect on main page select
 
 
+
+// Doughnut Chart for Weekly Goals in charity
 
 var dd1 = [
   {
@@ -39,8 +39,59 @@ var options = {
   percentageInnerCutout : 75
 };
 var dougnut1 = new Chart(document.getElementById("weeklyGoal").getContext("2d")).Doughnut(dd1,options);
-var dougnut2 = new Chart(document.getElementById("weeklyGoal2").getContext("2d")).Doughnut(dd1,options);
+var dougnut2 = new Chart(document.getElementById("weeklyGoal4").getContext("2d")).Doughnut(dd1,options);
 
 
+
+var dd2 = [
+  {
+    value: 20,
+    color:"#4eddc1"
+  },
+  {
+    value : 80,
+    color : "#E2EAE9"
+  }
+]
+var options = {
+  percentageInnerCutout : 75
+};
+var dougnut3 = new Chart(document.getElementById("weeklyGoal2").getContext("2d")).Doughnut(dd2,options);
+var dougnut4 = new Chart(document.getElementById("weeklyGoal5").getContext("2d")).Doughnut(dd2,options);
    
-///////tab refresh
+
+
+var dd3 = [
+  {
+    value: 75,
+    color:"#4eddc1"
+  },
+  {
+    value : 25,
+    color : "#E2EAE9"
+  }
+]
+var options = {
+  percentageInnerCutout : 75
+};
+var dougnut5 = new Chart(document.getElementById("weeklyGoal3").getContext("2d")).Doughnut(dd3,options);
+var dougnut6 = new Chart(document.getElementById("weeklyGoal6").getContext("2d")).Doughnut(dd3,options);
+
+
+
+
+
+
+
+
+
+
+
+///////main page select refresh
+
+
+document.getElementById("category").onchange = function() {
+        if (this.selectedIndex!==0) {
+            window.location.href = this.value;
+        }        
+    };
